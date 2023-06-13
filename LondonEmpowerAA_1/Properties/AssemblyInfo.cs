f0506debd934e6aac0e173a80826da11133852c2
aayepe <angela.ayepe@paramount.com>
2023-06-13 14:25:53 +0100

    Add project files.

---
+++ LondonEmpowerAA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LondonEmpowerAA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("LondonEmpowerAA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9F91FF56-FDC3-4B7C-8AF1-4ED71B468B33")]
+[assembly: AssemblyVersion("1.0.0.0")]
