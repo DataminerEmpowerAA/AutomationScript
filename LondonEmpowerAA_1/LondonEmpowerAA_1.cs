290942b77a1aa0d28f18be0569573cf420e47922
aayepe <angela.ayepe@paramount.com>
2023-06-13 14:36:05 +0100

    say hello

--- LondonEmpowerAA_1/LondonEmpowerAA_1.cs
+++ LondonEmpowerAA_1/LondonEmpowerAA_1.cs
@@ -19,7 +19,7 @@ namespace LondonEmpowerAA_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World from BT Tower");
 		}
 	}
 }
